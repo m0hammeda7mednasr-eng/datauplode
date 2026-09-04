7dd67e648eb4dcf8154aa2b417a02deb727b23ce
Mohammed Ahmed <m0hammeda7mednasr@gmail.com>
2026-09-04 14:41:23 +0300

    Start database-title catalog matcher

--- server-runtime.ts
+++ server-runtime.ts
@@ -1,3 +1,4 @@
 import './src/server/catalog-runtime-hotfix.js';
 import './src/server/catalog-assisted-match-worker.js';
+import './src/server/catalog-db-title-match-worker.js';
 await import('./server.js');
