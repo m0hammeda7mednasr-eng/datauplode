e8d08041e12ad9355943834f96bc7ac2f7343c14
Mohammed Ahmed <m0hammeda7mednasr@gmail.com>
2026-09-09 14:05:31 +0300

    Start exact Juniors vendor purge worker

--- server-runtime.ts
+++ server-runtime.ts
@@ -5,4 +5,5 @@ import './src/server/catalog-db-title-match-worker.js';
 import './src/server/catalog-sibling-match-worker.js';
 import './src/server/catalog-url-title-match-worker.js';
 import './src/server/catalog-source-authority-worker.js';
+import './src/server/juniors-vendor-purge-worker.js';
 await import('./server.js');
